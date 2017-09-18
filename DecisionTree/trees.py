a93a4ad8cf212a9f1501e8976f26599e10b486e3
杨清帅 <doitnow@doitnowdeMacBook-Pro.local>
2017-09-18 20:56:41 +0800

    使用matplotlib绘制决策树，并使用隐形眼镜类型数据验证决策树的分类功能

--- DecisionTree/trees.py
+++ DecisionTree/trees.py
@@ -2,6 +2,7 @@
 
 from math import log
 import operator
+import matplotlib.pyplot as plt
 
 def calcShannonEnt(dataSet):
     '''
@@ -79,10 +80,11 @@ def majorityCnt(classList):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-def createTree(dataSet, labels):
+def createTree(dataSet, oriLabels):
     '''
     构造决策树
     '''
+    labels = oriLabels[:]
     classList = [example[-1] for example in dataSet]
     # 如果类别完全相同则递归结束，划分完成
     if classList.count(classList[0]) == len(classList):
@@ -101,8 +103,123 @@ def createTree(dataSet, labels):
         myTree[bestFeatLabel][val] = createTree(splitDataSet(dataSet, bestFeat, val), subLabels)
     return myTree
 
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, 
+                            xy=parentPt, 
+                            xycoords='axes fraction', 
+                            xytext=centerPt, 
+                            textcoords='axes fraction',
+                            va="center",
+                            ha="center",
+                            bbox=nodeType,
+                            arrowprops=arrow_args)
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    sencondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in sencondDict.keys():
+        if type(sencondDict[key]).__name__ == 'dict':
+            plotTree(sencondDict[key], cntrPt, str(key))  
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(sencondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD                          
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+def getNumLeafs(myTree):
+    '''
+    获取叶子节点个数
+    '''
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    sencondDict = myTree[firstStr]
+    for key in sencondDict.keys():
+        if type(sencondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(sencondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+def getTreeDepth(myTree):
+    '''
+    获取树的深度
+    '''
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    sencondDict = myTree[firstStr]
+    for key in sencondDict.keys():
+        if type(sencondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(sencondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+def classify(inputTree, featLabels, testVec):
+    '''
+    使用建好的树分类
+    '''
+    firstStr = inputTree.keys()[0]
+    sencondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in sencondDict.keys():
+        if testVec[featIndex] == key:
+            if type(sencondDict[key]).__name__ == 'dict':
+                classLabel = classify(sencondDict[key], featLabels, testVec)
+            else:
+                classLabel = sencondDict[key]
+    return classLabel
+
+def storeTree(inputTree, filename):
+    '''
+    使用pickle存储树
+    '''
+    import pickle
+    fw = open(filename, 'w')
+    pickle.dump(inputTree, fw)
+    fw.close()
+
+def grabTree(filename):
+    '''
+    从pickle文件中读取树
+    '''
+    import pickle
+    fr = open(filename)
+    return pickle.load(rf)
+
 if __name__ == '__main__':
-    myDat, labels = createDataSet()
-    print calcShannonEnt(myDat)
-    print chooseBestFeatureToSplit(myDat)
-    print createTree(myDat, labels)
+    fr = open('lenses.txt')
+    lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+    lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    lensesTree = createTree(lenses, lensesLabels)
+    print lensesTree
+    createPlot(lensesTree)
+    print classify(lensesTree, lensesLabels, ['presbyopic', 'hyper', 'no', 'normal'])
